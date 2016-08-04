f9ea7ccbed86ed23ec35f05fe523e25442112464
Andrew Creahan <andrew@creahan.co.nz>
2016-08-05 10:57:41 +1200

    Reduce Min & Max scan angle
    To avoid conflict with head

--- xv_11_laser_driver/src/xv11_laser.cpp
+++ xv_11_laser_driver/src/xv11_laser.cpp
@@ -124,8 +124,8 @@ namespace xv_11_laser_driver {
 
 	    boost::asio::read(serial_,boost::asio::buffer(&raw_bytes[2], 1978));
 
-	    scan->angle_min = 0.0;
-	    scan->angle_max = 2.0*M_PI;
+	    scan->angle_min = 0.1*M_PI;
+	    scan->angle_max = 1.9*M_PI;
 	    scan->angle_increment = (2.0*M_PI/360.0);
 	    scan->range_min = 0.06;
 	    scan->range_max = 5.0;
